964859c8edd6d604f6d8dc63f2084a2b70bc4850
elogeek <elodiechristin@gmail.com>
2022-01-06 16:48:05 +0100

    suite

--- ProjectDockMacOs/assets/js/components/Icons.js
+++ ProjectDockMacOs/assets/js/components/Icons.js
@@ -1,20 +1,31 @@
 import {MacDock} from "./MacDock";
 
-let imgIcon = [
-    {imgIcon:"./assets/img/androidStudio.png", title:"img 1", tooltip:"Android Studio"}
-]
 
 const Link = function(link, title,target,tooltip) {
     this.href = link;
     this.alt = title;
     this.target = "_blank";
-
+    this.tooltip = tooltip;
 }
-new Link("","")
-export let Icon = function () {}
 
-    // create a icon
-    this.create = function () {
+new Link("./assets/img/icon1.png","img1","_blank","Android Studio");
+new Link("./assets/img/icon2.png", "img2", "_blank", "AppStore");
+new Link("./assets/img/icon3.png", "img3","_blank", "Blender");
+new Link("./assets/img/icon4.png", "img4", "_blank", "Calculator");
+new Link("./assets/img/icon5.png", "img5", "_blank", "Calendar");
+new Link("./assets/img/icon6.png", "img6", "_blank", "ChatDev");
+new Link("./assets/img/icon7.png", "img7", "_blank", "Discord");
+new Link("./assets/img/icon8.png", "img8", "_blank", "Docker");
+new Link("./assets/img/icon9.png", "img9", "_blank", "Duolingo");
+new Link("./assets/img/icon10.png", "img10", "_blank", "Firefox");
+new Link("./assets/img/icon11.png", "img11", "_blank", "Github");
+new Link("./assets/img/icon12.png", "img12", "_blank", "Google Translate");
+new Link("./assets/img/icon13.png", "img13", "_blank", "Intellij Idea");
+new Link("./assets/img/icon14.png", "img14", "_blank", "Spotify");
+new Link("./assets/img/icon15.png", "img15", "_blank", "Steam");
+new Link("./assets/img/icon16.png", "img16", "_blank", "Terminal");
+new Link("./assets/img/icon17.png", "img17", "_blank", "Google Docs");
 
 
-    }
+
+
