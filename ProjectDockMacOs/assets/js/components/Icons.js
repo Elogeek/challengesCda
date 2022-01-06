a8bfee9425a614c248bccc7320f644923bc1c665
elogeek <elodiechristin@gmail.com>
2022-01-06 15:46:54 +0100

    suite

---
+++ ProjectDockMacOs/assets/js/components/Icons.js
@@ -0,0 +1,20 @@
+import {MacDock} from "./MacDock";
+
+let imgIcon = [
+    {imgIcon:"./assets/img/androidStudio.png", title:"img 1", tooltip:"Android Studio"}
+]
+
+const Link = function(link, title,target,tooltip) {
+    this.href = link;
+    this.alt = title;
+    this.target = "_blank";
+
+}
+new Link("","")
+export let Icon = function () {}
+
+    // create a icon
+    this.create = function () {
+
+
+    }
