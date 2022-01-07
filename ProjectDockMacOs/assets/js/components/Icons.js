5e712c0e52228a8d1b800204f09e5e0a7b629d7a
elogeek <elodiechristin@gmail.com>
2022-01-07 18:01:32 +0100

    suite

--- ProjectDockMacOs/assets/js/components/Icons.js
+++ ProjectDockMacOs/assets/js/components/Icons.js
@@ -1,11 +1,9 @@
 import MacDock from "./MacDock";
 
-
 const Link = function(link, title,target) {
     this.href = link;
     this.title = title;
     this.target = "_blank";
-
 }
 
 new Link("./assets/img/androidS.png","img1","_blank");
