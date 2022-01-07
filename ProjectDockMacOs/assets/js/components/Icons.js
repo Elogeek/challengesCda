677be44b758e7631253ee7d84f9433f7f1128728
elogeek <elodiechristin@gmail.com>
2022-01-07 09:42:19 +0100

    suite

--- ProjectDockMacOs/assets/js/components/Icons.js
+++ ProjectDockMacOs/assets/js/components/Icons.js
@@ -21,7 +21,6 @@ new Link("./assets/img/icon11.png", "img11", "_blank", "Github");
 new Link("./assets/img/icon12.png", "img12", "_blank", "Google Translate");
 new Link("./assets/img/icon13.png", "img13", "_blank", "Intellij Idea");
 new Link("./assets/img/icon14.png", "img14", "_blank", "Spotify");
-new Link("./assets/img/icon15.png", "img15", "_blank", "Steam");
 new Link("./assets/img/icon16.png", "img16", "_blank", "Terminal");
 new Link("./assets/img/icon17.png", "img17", "_blank", "Photos");
 new Link("./assets/img/icon18.png", "img18", "_blank", "Manga");
